dc2786d06d830c2bd6e9e63466bd713e711e14f1
khvorostianenko <mikhail.khvorostianenko@gmail.com>
2016-09-23 11:01:24 +0300

    1.5 some mod

--- www/config/config.php
+++ www/config/config.php
@@ -20,9 +20,4 @@ Config::set('default_action', 'index');
 //Config::set('db.password', '');
 //Config::set('db.db_name', '');
 
-Config::set('db.host', '195.191.24.196');
-Config::set('db.user', 'xgnbpama_mod4misha');
-Config::set('db.password', 'waU=DQ]la*C;');
-Config::set('db.db_name', 'xgnbpama_mod4khvorostianenko');
 
-Config::set('salt','rijeeh35sda766eue');
