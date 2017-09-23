007ff57f86bafafecf29318db7ded3c132680ca1
Mikhail <mikhail.khvorostianenko@gmail.com>
2017-09-23 18:05:35 +0300

    bugs fix

--- www/config/config.php
+++ www/config/config.php
@@ -15,8 +15,8 @@ Config::set('default_language', 'en');
 Config::set('default_controller', 'pages');
 Config::set('default_action', 'index');
 
-//Config::set('db.host', '');
-//Config::set('db.user', '');
-//Config::set('db.password', '');
-//Config::set('db.db_name', '');
+Config::set('db.host', '127.0.0.1');
+Config::set('db.user', 'root');
+Config::set('db.password', '');
+Config::set('db.db_name', 'mod4khvorostianenko');
 
--- www/webroot/js/onexit.js
+++ www/webroot/js/onexit.js
@@ -1,77 +1,59 @@
-//Это рабочий вариант (#2)
-function Unloader(){
+// Код для запрета ухода со страницы
 
-    var o = this;
-
-    this.unload = function(evt)
-    {
-        var message = "Вы уверены, что хотите покинуть страницу оформления заказа?";
-        if (typeof evt == "undefined") {
-            evt = window.event;
-        }
-        if (evt) {
-            evt.returnValue = message;
-        }
-        return message;
-    }
-
-    this.resetUnload = function()
-    {
-        $(window).off('beforeunload', o.unload);
-
-         setTimeout(function(){
-            $(window).on('beforeunload', o.unload);
-        }, 2000);
-    }
-
-    this.init = function()
-    {
-
-        $(window).on('beforeunload', o.unload);
-
-        $('a').on('click', function(){o.resetUnload});
-
-            $(document).on('submit', 'form', function(){o.resetUnload});
-        $(document).on('keydown', function(event){
-            if((event.ctrlKey && event.keyCode == 116) || event.keyCode == 116){
-                o.resetUnload;
-            }
-        }
-        );
-    }
-    this.init();
-}
-
-$(function(){
-    if(typeof window.obUnloader != 'object')
-    {
-        window.obUnloader = new Unloader();
-    }
-})
-
-
-//Это рабочий вариант (#1)
-//window.onbeforeunload = function()
-//{
-//    return confirm('Вы хотите покинуть сайт?')
-//}
-
-//Мусор 3
-//$(document).ready(function () {
-//  $(window).on('beforeunload', function() {
-//   return " тест "; // СООБЩЕНИЕ ПРИ ЗАКРЫТИИ!
+// Вариант №1
+// function Unloader(){
 //
-//  $('a').click(function() {
-//    $(window).off('beforeunload');
-//  });
-//});
+//     var o = this;
+//
+//     this.unload = function(evt)
+//     {
+//         var message = "Вы уверены, что хотите покинуть страницу оформления заказа?";
+//         if (typeof evt == "undefined") {
+//             evt = window.event;
+//         }
+//         if (evt) {
+//             evt.returnValue = message;
+//         }
+//         return message;
+//     }
+//
+//     this.resetUnload = function()
+//     {
+//         $(window).off('beforeunload', o.unload);
+//
+//          setTimeout(function(){
+//             $(window).on('beforeunload', o.unload);
+//         }, 2000);
+//     }
+//
+//     this.init = function()
+//     {
+//
+//         $(window).on('beforeunload', o.unload);
+//
+//         $('a').on('click', function(){o.resetUnload});
+//
+//             $(document).on('submit', 'form', function(){o.resetUnload});
+//         $(document).on('keydown', function(event){
+//             if((event.ctrlKey && event.keyCode == 116) || event.keyCode == 116){
+//                 o.resetUnload;
+//             }
+//         }
+//         );
+//     }
+//     this.init();
+// }
+//
+// $(function(){
+//     if(typeof window.obUnloader != 'object')
+//     {
+//         window.obUnloader = new Unloader();
+//     }
+// })
 
-//Мусор 1
-//window.onbeforeunload = function(){
-//    return confirm('Точно хотите выйти?');
-//}
 
-//Мусор 2
-//window.onbeforeunload = function() {
-//    return "Что-нибудь сообщить пользователю";
-//}
+// Вариант №2
+// window.onbeforeunload = function()
+// {
+//    return confirm('Вы хотите покинуть сайт?')
+// }
